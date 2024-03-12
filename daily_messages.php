e3801e658085adb2a34ecd1c97a85413945766c5
Sergey Svist <sergeysvist557@gmail.com>
2024-03-12 17:20:45 +0200

    add: daily messages service

--- daily_messages.php
+++ daily_messages.php
@@ -1,25 +1,19 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once 'services/userinfo_service.php';
 require_once 'services/chat_service.php';
-require_once 'services/login_service.php';
-require_once 'services/datareplace_service.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 use danog\MadelineProto\API;
 use danog\MadelineProto\Settings;
 use danog\MadelineProto\Settings\AppInfo;
-use danog\MadelineProto\Tools;
-use MongoDB\Collection;
 
-//Connect to MongoDb
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $client = new MongoDB\Client(
     'mongodb+srv://'.urlencode($_ENV['MDB_USER']).':'.urlencode($_ENV['MDB_PASS']).'@'.$_ENV['ATLAS_CLUSTER_SRV'].'/?retryWrites=true&w=majority&appName=Cluster0'
 );
 $collection = $client->selectCollection($_ENV['MDB_DATABASE'], $_ENV['MDB_COLLECTION']);
 
-//connect to MadelineProto
 $settings = new Settings;
 
 $settings->setAppInfo((new AppInfo)
@@ -27,20 +21,7 @@ $settings->setAppInfo((new AppInfo)
     ->setApiHash($_ENV['TG_APIHASH'])
 );
 
-//for replace data in user active devices
-//replaceDeviceForAuth($settings, "Telegram Web Test");
-/*addHttpProxy($settings,
-    [
-        'address'  => '0.0.0.0',
-        'port'     =>  2343,
-    ]);*/
-
-
 $MadelineProto = new API('session.madeline', $settings);
-//Login
-autoLogin($MadelineProto);
 
-
-getMessagesFromAllDialogsAndUploadInDb($MadelineProto, $collection);
-
-
+$last12hours = strtotime("-12 Hours");
+getMessagesFromAllDialogsAndUploadInDb($MadelineProto, $collection, $last12hours);
--- services/chat_service.php
+++ services/chat_service.php
@@ -6,36 +6,43 @@ use MongoDB\Collection;
 
 function getAllUserDialogs(AbstractAPI $MadelineProto): array
 {
-    return $MadelineProto->getDialogIds();
+    return $MadelineProto->getFullDialogs();
 }
 
-function getMessagesFromAllDialogsAndUploadInDb(AbstractAPI $MadelineProto, Collection $collection): int
+function getMessagesFromAllDialogsAndUploadInDb(AbstractAPI $MadelineProto, Collection $collection, int $min_date = 0): int
 {
     $dialogs = getAllUserDialogs($MadelineProto);
     $inserted_count = 0;
 
     foreach ($dialogs as $peer){
-        $inserted_count += getMessagesAndUploadInDb($MadelineProto, $collection, $peer);
+        $inserted_count += getMessagesAndUploadInDb($MadelineProto, $collection, $peer, $min_date);
     }
+
 
     return $inserted_count;
 }
 
-function getMessagesAndUploadInDb(AbstractAPI $MadelineProto, Collection $collection, int $peer): int
+function getMessagesAndUploadInDb(AbstractAPI $MadelineProto, Collection $collection, array $peer, int $min_date = 0): int
 {
     $offset_id = 0;
     $inserted_count = 0;
+    if($min_date!=0) {
+        $top_message = $MadelineProto->messages->getMessages(id: [$peer['top_message']]);
+        $top_message_date = 0;
+        if (isset($top_message['messages'][0]['date']))
+            $top_message_date = $top_message['messages'][0]['date'];
+        else
+            return 0;
+        if ($min_date > $top_message_date)
+            return 0;
+    }
     do {
         //get messages from user dialog
-        $messages = $MadelineProto->messages->getHistory([
-                'peer' => $peer,
+        $messages = $MadelineProto->messages->search([
+                'peer' => $peer['peer'],
                 'offset_id' => $offset_id,
-                'offset_date' => 0,
-                'add_offset' => 0,
                 'limit' => 100,
-                'max_id' => 0,
-                'min_id' => 0,
-                'hash' => 0 ]
+                'min_date' => $min_date]
         );
 
         if (count($messages['messages']) == 0) break;
@@ -50,3 +57,4 @@ function getMessagesAndUploadInDb(AbstractAPI $MadelineProto, Collection $collec
 
     return $inserted_count;
 }
+
